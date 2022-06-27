c12aa049a22cda2ece3359d5d488b36c3f5fafad
Eugene Bell <eugene_bell@yahoo.com>
2022-06-27 20:45:25 +0100

    zara first program

---
+++ Zara-toca-playground/zara.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	urmom := "i like how unicorns poo rainbows"
+	fmt.Println(urmom)
+	fmt.Println(urmom)
+	fmt.Println(urmom)
+	fmt.Println("Zara wrote ", urmom)
+}
